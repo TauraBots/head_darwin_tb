9b199f8144f8034b3871793056d8bfd074b357da
Milton Brenner Machado Matoso <miltonbmmatoso@gmail.com>
2016-04-01 15:29:15 -0300

    Update teste.sh

--- teste.sh
+++ teste.sh
@@ -2,19 +2,28 @@
 
 cd ~/catkin_ws
 source /opt/ros/indigo/setup.bash
+
+#kill ros processes if already running
+sudo killall roslaunch
 sudo killall -9 rosmaster
+
+#just to do the setup, sometimes we have problems with this
 catkin_make
 source devel/setup.bash
 sudo chmod 777 /dev/ttyUSB0
 cd ~/catkin_ws/src
 
+#launching the controller that connects the dynamixel motors
+#with the opencm and the opencm with the computer
 roslaunch head_darwin_tb/launch/dx_controller.launch &
 sleep 5
-xdotool key enter #sudo apt-get install xdotools
 
+#it actually start the motors that are defined on 
+#head_darwin_tb/param/parm_head.yaml in our code!!!
 roslaunch head_darwin_tb/launch/start_controller.launch &
-sleep5
-xdotool key enter
+sleep 2
 
-python head_darwin_tb/src/trajectory_client.py
+#our test code!
+python ~/catkin_ws/src/head_darwin_tb/src/trajectory_client.py
+
 exit
